9b7163cd216b52b05ca1f1479b0e1243b7c367a0
Nimira43 <n.rabone72@aol.com>
2024-07-20 09:03:20 +0100

    Start to set up movement and input

--- game.py
+++ game.py
@@ -6,15 +6,23 @@ class Game:
     pygame.display.set_caption('Sky Islands')
     self.screen = pygame.display.set_mode((640, 480))
     self.clock = pygame.time.Clock()
+
     self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+    self.img_pos = [160, 260]
+    self.movement = [False, False]
 
   def run(self):
     while True:
-      self.screen.blit(self.img, (100, 200))
+      self.screen.blit(self.img, self.img_pos)
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
           sys.exit()
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_UP:
+            self.movement[0] = True
+          if event.key == pygame.K_DOWN:
+            self.movement[1] = True
 
       pygame.display.update()
       self.clock.tick(60)
