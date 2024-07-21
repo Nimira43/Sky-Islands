5e227a0d40006cf1b9122d9564416ab122882ccd
Nimira43 <n.rabone72@aol.com>
2024-07-21 08:24:25 +0100

    Moving cloud

--- game.py
+++ game.py
@@ -3,17 +3,26 @@ import pygame, sys
 class Game:
   def __init__(self):
     pygame.init()
+    
     pygame.display.set_caption('Sky Islands')
     self.screen = pygame.display.set_mode((640, 480))
+    
     self.clock = pygame.time.Clock()
 
     self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+    self.img.set_colorkey((0, 0, 0))
+    
     self.img_pos = [160, 260]
     self.movement = [False, False]
 
+    self.collision_area = pygame.Rect(50, 50, 300, 50)
+
   def run(self):
     while True:
+      self.screen.fill((14, 219, 248))
+      self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
       self.screen.blit(self.img, self.img_pos)
+      
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
@@ -23,6 +32,11 @@ class Game:
             self.movement[0] = True
           if event.key == pygame.K_DOWN:
             self.movement[1] = True
+        if event.type == pygame.KEYUP:
+          if event.key == pygame.K_UP:
+            self.movement[0] = False
+          if event.key == pygame.K_DOWN:
+            self.movement[1] = False
 
       pygame.display.update()
       self.clock.tick(60)
