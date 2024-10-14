b40c5391aa6a2c4c557999ee6b5ebfc6c4de9fff
Nimira43 <n.rabone72@aol.com>
2024-10-14 11:44:25 +0100

    Using for loop to map out tiles

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -3,3 +3,9 @@ class Tilemap:
     self.tile_size = tile_size
     self.tilemap = {}
     self.offgrid_tiles = []
+
+    for i in range(10):
+      self.tile[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
+      self.tile['10' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
+
+    
