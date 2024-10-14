2917e4be927261cea5219a2af3887b74c075ccbd
Nimira43 <n.rabone72@aol.com>
2024-10-14 11:24:04 +0100

    Player sprite brought onto screen

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from scripts.utils import load_image
 from scripts.entities import PhysicsEntity
 
 class Game:
@@ -8,28 +9,35 @@ class Game:
     pygame.display.set_caption('Sky Islands')
     self.screen = pygame.display.set_mode((640, 480))
     self.clock = pygame.time.Clock()
-    
+
+    self.movement = [False, False]
+
+    self.assets = {
+      'player': load_image('entities/player.png')
+    }
     self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+    self.player.render(self.screen)
 
   def run(self):
     while True:
       self.screen.fill((14, 219, 248))
       
-      
+      self.player.update((self.movement[1] - self.movement[0], 0))
+      self.player.render(self.screen)
 
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
           sys.exit()
         if event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_UP:
+          if event.key == pygame.K_LEFT:
             self.movement[0] = True
-          if event.key == pygame.K_DOWN:
+          if event.key == pygame.K_RIGHT:
             self.movement[1] = True
         if event.type == pygame.KEYUP:
-          if event.key == pygame.K_UP:
+          if event.key == pygame.K_LEFT:
             self.movement[0] = False
-          if event.key == pygame.K_DOWN:
+          if event.key == pygame.K_RIGHT:
             self.movement[1] = False
 
       pygame.display.update()
